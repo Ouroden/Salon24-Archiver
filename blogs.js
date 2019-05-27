e38447f1580c2ed733364e07dc1dd0e3bea9f1a0
Tomasz Wis <tomaszrwis@gmail.com>
2019-05-27 12:44:27 +0200

    add basic db analysis

--- blogs.js
+++ blogs.js
@@ -6,3 +6,46 @@ function last(N) {
 function one() {
     return db.Blogs.findOne();
 }
+
+function blogCount() {
+    return db.Blogs.aggregate([
+        { $group: { _id: null, count: {$sum: 1} } }
+    ]);
+}
+
+function withArticles() {
+    return db.Blogs.aggregate([
+        { $match: { "articles_amount" : {$gt : 0}} },
+        { $group: { _id: null, count: {$sum: 1} } }
+    ]);
+}
+
+function withNoArticles() {
+    return db.Blogs.aggregate([
+        { $match: { "articles_amount" : 0 } },
+        { $group: { _id: null, count: {$sum: 1} } }
+    ]);
+}
+
+function totalNumberOfArticles() {
+    return db.Blogs.aggregate([
+        { $match: { "articles_amount" : {$gt : 0}} },
+        { $group: { _id: null, total: {$sum: "$articles_amount"} } }
+    ]);
+}
+
+function totalNumberOfViews() {
+    return db.Blogs.aggregate([
+        { $match: { "views" : {$gt : 0}} },
+        { $group: { _id: null, total: {$sum: "$views"} } }
+    ]);
+}
+
+function totalNumberOfFollowers() {
+    return db.Blogs.aggregate([
+        { $match: { "followers" : {$gt : 0}} },
+        { $group: { _id: null, total: {$sum: "$followers"} } }
+    ]);
+}
+
+
