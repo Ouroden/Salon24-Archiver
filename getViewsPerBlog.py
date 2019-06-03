a0e3da87adfeaa33e575ae9f938fdcffafd13fac
Tomasz Wis <tomaszrwis@gmail.com>
2019-06-03 12:14:57 +0200

    add getCommentsPerArticle.py

---
+++ getViewsPerBlog.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+from pymongo import MongoClient
+from pprint import pprint
+
+def main(blogs):
+
+    pipeline = [
+        {"$unwind": "$articles"},
+        {"$group": {
+            "_id": "$blog_name",
+            "views": {"$sum": "$articles.views"}
+        }},
+        {"$sort": {"views": -1}}
+    ]
+
+    cursor = blogs.aggregate(pipeline)
+
+    result = list(cursor)
+
+    pprint(result)
+    #pprint(result[:10])
+
+if __name__ == '__main__':
+    client = MongoClient('localhost:27017')
+    db = client.Salon
+    blogs = db["Blogs"]
+
+    try:
+        main(blogs)
+    except Exception as e:
+        print(str(e))
+
+
